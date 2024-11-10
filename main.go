75915de87c66b81b43e5a7181d08cc0ffd7d5f24
mohammad zeya ahmad <mohammadzeyaahmad@gmail.com>
2024-11-10 22:31:47 +0530

    user session added and main.go configured

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+
+	"github.com/MohammadZeyaAhmad/bank/api"
+	db "github.com/MohammadZeyaAhmad/bank/db/sqlc"
+
+	"github.com/MohammadZeyaAhmad/bank/util"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot load config")
+	}
+
+	connPool, err := pgxpool.New(context.Background(), config.DBSource)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot connect to db")
+	}
+
+   store := db.NewStore(connPool)
+
+   server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot create server")
+	}
+
+	err = server.Start(config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot start server")
+	}
+}
